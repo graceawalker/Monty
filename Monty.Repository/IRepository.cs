f60034112291a7644c7ef5ed1dc3ddbb592ed76a
graceawalker <grace_blonde@hotmail.com>
2012-06-18 12:19:46 +0100

    Added repo

---
+++ Monty.Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monty.Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        void CreateNew(T toCreate);
+    }
+}
---
+++ Monty.Repository/PayPeriod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson;
+
+namespace Monty.Repository
+{
+    public class PayPeriod
+    {
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement]
+        public string Name { get; set; }
+
+       [BsonDateTimeOptions]
+        public DateTime StartDate { get; set; }
+
+        [BsonDateTimeOptions]
+        public DateTime EndDate { get; set; }
+    }
+}
